7efa4c2a158fa020a84896cfb69f9b49b4a7028b
Ekaterina Ledeneva <cthfvrl@gmail.com>
2019-11-29 18:09:09 +0300

    Modified Map and Unit

--- Source/Map.h
+++ Source/Map.h
@@ -1,14 +1,17 @@
+#pragma once
 #include <vector>
 #include "Unit.h"
 
 class Map {
 public:
-    Map();
-    ~Map();
+    Map() : map(std::vector<std::vector<Unit> >(width, std::vector<Unit> (height))) {};
+
     std::vector<std::vector<Unit> > const& getMap(){
         return map;
     }
 
 private:
+    const int width = 10;
+    const int height = 10;
     std::vector<std::vector<Unit> > map;
 };
--- Source/Unit.h
+++ Source/Unit.h
@@ -1,5 +1,15 @@
+#pragma once
+#include "../JuceLibraryCode/JuceHeader.h"
+#include <cstdlib>
+#include <ctime>
+
 class Unit {
 public:
+    Unit(){
+        std::srand(unsigned(std::time(0)));
+        colour = Colour::fromRGB(std::rand(), std::rand(), std::rand());
+    }
+
     Colour const& getColour(){
         return colour;
     }
